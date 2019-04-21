5610c4db2658285ddbdf317c699d9ef498cd4a2e
Ricky Romero <rickyromero@Rickys-MacBook-Pro.local>
2019-04-21 11:37:04 -0700

    Container comp

---
+++ app/src/components/layouts/Container.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  View,
+  ScrollView,
+  Dimensions
+} from "react-native";
+import { getTheme } from "../styles/colors";
+
+const { height } = Dimensions.get("window");
+const theme = getTheme();
+
+export default class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      screenHeight: height
+    };
+  }
+
+  onContentSizeChange = (contentWidth, contentHeight) => {
+    this.setState({ screenHeight: contentHeight });
+  };
+
+  render() {
+    const scrollEnabled = this.state.screenHeight > height;
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={styles.scrollview}
+          scrollEnabled={scrollEnabled}
+          onContentSizeChange={this.onContentSizeChange}
+        >
+          <View style={styles.content}>{this.props.children}</View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.backgroundColor
+  },
+  scrollview: {
+    flexGrow: 1
+  },
+  content: {
+    flexGrow: 1,
+    justifyContent: "space-between",
+    padding: 10
+  }
+});

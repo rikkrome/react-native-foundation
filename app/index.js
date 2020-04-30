4baf055752e4996fa9d46fd6ba75b470faccdc6f
ricky romero <contact@romeroricky.com>
2020-04-29 18:34:34 -0700

    moved old app folder

--- app/index.js
+++ app/index.js
@@ -1,13 +1,9 @@
 /**
  * @format
  */
-import { AppRegistry, YellowBox } from "react-native";
-import App from "./src";
-import { name as appName } from "./app.json";
 
-YellowBox.ignoreWarnings([
-  "Deprecation warning: moment construction falls back to js Date. This is discouraged and will be removed in upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.",
-  "Task orphaned for request ",
-  "Remote debugger is in a background tab which may cause apps to perform slowly"
-]);
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
 AppRegistry.registerComponent(appName, () => App);
